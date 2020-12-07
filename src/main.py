db691b9c2152637ff20830bfd0828c82cc3bd949
vladimirfomene <vladimirfomene@gmail.com>
2020-12-07 09:31:25 +0100

    change request.query to request.args

--- src/main.py
+++ src/main.py
@@ -13,20 +13,43 @@ CORS(app)
 # generate database schema
 Base.metadata.create_all(engine)
 
-@app.route('/customers')
+@app.route('/customers', methods=['GET'])
 def get_customers():
 
     # fetching from the database
     session = Session()
-    customer_objects = session.query(Customer).all()
-
-    # transforming into JSON-serializable objects
     schema = CustomerSchema(many=True)
+
+    if request.args.get("name") != None:
+        customer_objects = session.query(Customer) \
+        .filter(Customer.name.ilike('%' + request.args.get("name") + '%')) \
+        .all()
+
+        customers = schema.dump(customer_objects)
+        session.close()
+        return jsonify(customers)
+
+
+
+    customer_objects = session.query(Customer).all()
     customers = schema.dump(customer_objects)
 
     # serializing as JSON
     session.close()
-    return jsonify(customers.data)
+    return jsonify(customers)
 
-@app.route('/customers/<id>')
-def search_customers():
+@app.route('/loans', methods=['GET'])
+def search_loans_by_customer():
+
+    if request.args.get("customerId") != None:
+        session = Session()
+        customer_loans_objs = session.query(Loan) \
+        .join(Customer) \
+        .filter(Loan.customer_id == request.args.get("customerId")) \
+        .all()
+
+        schema = LoanSchema(many=True)
+        customer_loans = schema.dump(customer_loans_objs)
+
+        session.close()
+        return jsonify(customer_loans)
